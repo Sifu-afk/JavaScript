09ba8e6f5e4038fb359ebc9aa53085f0e0479ddf
Sifu-afk <gytisstravinskas11@gmail.com>
2024-12-25 02:01:24 +0200

    did simple questioneer using alert and promt

--- tests/1/script.js
+++ tests/1/script.js
@@ -10,17 +10,20 @@ const length = questions1.length
 
 console.log(length)
 
+//function running throu all of the questions
+
 for (let i = 0; i < questions1.length; i++){
 
-    let question = prompt(`${questions1[i].question}, ${questions1[i].answers.map((x) => x.text)}`)
+    let answer = prompt(`${questions1[i].question}, ${questions1[i].answers.map((x) => x.text)}`)
+    var x = Number(answer)
+    let position = x - 1
 
-    if(question == questions1[i].answers.map((x) => x.isCorrect)){
-        alert('you are correct')
-    }else{
-        alert('INCORECT')
-    }
+   if(questions1[i].answers[position].isCorrect){
+    alert(`correct`)
+   }else{
+    alert(`incorrect`)
+   }
 
-   
 }
 
 
